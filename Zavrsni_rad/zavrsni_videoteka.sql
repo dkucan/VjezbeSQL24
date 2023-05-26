73abd2ccadd0b8d49d967d3885dbc38e9e70d280
Darko Kučan <131719007+dkucan@users.noreply.github.com>
2023-05-26 20:30:17 +0200

    unos clanova u evidenciju videoteke

--- Zavrsni_rad/zavrsni_videoteka.sql
+++ Zavrsni_rad/zavrsni_videoteka.sql
@@ -39,3 +39,12 @@ Create table POSUDBA (
 	alter table POSUDBA add foreign key (CLAN) references clan (sifra);
 	alter table POSUDBAKAZETA add foreign key (KAZETA) references kazeta (sifra);
 	alter table POSUDBAKAZETA add foreign key (POSUDBA) references posudba (sifra);
+
+	insert into CLAN (Ime, Prezime, Adresa, Mobitel, OIB, Datum_uclanjenja) values ('Pero', 'Djetlic', 'Djetliceva_5', '09112345678', '20751953568', '2022-12-10');
+	insert into CLAN (Ime, Prezime, Adresa, Mobitel, OIB, Datum_uclanjenja) values ('Patak', 'Dodo', 'Dodina_16', '09212345678', '20751953568', '2022-10-10');
+	insert into CLAN (Ime, Prezime, Adresa, Mobitel, OIB, Datum_uclanjenja) values ('Zekoslav', 'Mrkva', 'Mrkvina_22', '09512345678', '20751953568', '2022-08-23');
+	insert into CLAN (Ime, Prezime, Adresa, Mobitel, OIB, Datum_uclanjenja) values ('Marko', 'Markovic', 'Markoviceva_35', '09712345678', '20751953568', '2022-11-17');
+	Insert into CLAN (Ime, Prezime, Adresa, Mobitel, OIB, Datum_uclanjenja) values ('Pero', 'Peric', 'Periceva_9', '09812345678', '20751953568', '2022-04-11');
+	Insert into CLAN (Ime, Prezime, Adresa, Mobitel, OIB, Datum_uclanjenja) values ('Ana', 'Anic', 'Anina_15', '09912345678', '20751953568', '2022-04-23');
+	Insert into CLAN (Ime, Prezime, Adresa, Mobitel, OIB, Datum_uclanjenja) values ('Katarina', 'Peric', 'Periceva_12', '09923456789', '20751953568', '2022-04-16');
+	Insert into CLAN (Ime, Prezime, Adresa, Mobitel, OIB, Datum_uclanjenja) values ('Ana', 'Markovic', 'Markoviceva_35', '09712345678', '20751953568', '2022-11-17');
