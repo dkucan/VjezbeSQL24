6235ddb355e7216326714cbcb4ffc7c9395eb8c9
Darko Kučan <131719007+dkucan@users.noreply.github.com>
2023-06-19 12:13:42 +0200

    dopuna vjezbasql2

--- VJEZBE_SQL/vjezba2.sql
+++ VJEZBE_SQL/vjezba2.sql
@@ -31,7 +31,7 @@ create table DEČKO(
 create table ZARUČNICA (
 	Šifra int primary key identity(1,1) not null,
 	Narukvica int,
-	Bojakose varchar(37) not null,
+	Boja_kose varchar(37) not null,
 	Novčića decimal(15,9),
 	Lipa decimal(15,8) not null,
 	Indiferentno bit not null
@@ -67,7 +67,7 @@ create table NEPRIJATELJ (
 
 create table BRAT(
 	Šifra int primary key identity(1,1) not null,
-	Suknja varchar,
+	Suknja varchar(47),
 	Ogrlica int not null,
 	Asocijalno bit not null,
 	Neprijatelj int not null
@@ -79,3 +79,78 @@ alter table BRAT add foreign key (NEPRIJATELJ) references neprijatelj (šifra);
 alter table PRIJATELJ add foreign key (SVEKAR) references svekar (šifra);
 alter table DEČKO_ZARUČNICA add foreign key (DEČKO) references dečko (šifra);
 alter table DEČKO_ZARUČNICA add foreign key (ZARUČNICA) references zaručnica (šifra);
+
+
+	select * from ZARUČNICA;
+
+	insert into ZARUČNICA (Boja_kose, lipa, Indiferentno) 
+		values
+			('crvena', '40.37', '0'),
+			('plava', '39.99', '0'),
+			('smeđa', '42.99', '0');
+
+	select * from DEČKO;
+
+		insert into DEČKO (Indiferentno, Vesta, Asocijalno)
+			values
+				('1', 'Smeđa pamučna', '0'),
+				('0', 'Crna pamučna', '1'),
+				('0', 'Bijela pamučna', 0);
+	
+	select * from DEČKO_ZARUČNICA;
+	
+	insert into DEČKO_ZARUČNICA (dečko, zaručnica) 
+		values
+			(1,1),
+			(2,2),
+			(3,3);
+		
+		select * from CURA;
+
+		insert into CURA (Haljina, Drugi_puta, Suknja, Narukvica, Introvertno, Majica, Dečko)
+			values
+				('kratka', '2023-04-24', 'kratka', 1, 0, 'kratka', 1),
+				('kratka', '2023-04-24', 'dugačka', 2,0, 'dugačka', 2 ),
+				('dugačka', '2023-04-24', 'kratka', 1, 0, 'kratka', 3 );
+
+		select * from NEPRIJATELJ;
+		
+		insert into NEPRIJATELJ (Majica, Haljina, Model_naočala, Kuna, Jmbag, Cura)
+			values
+				('široka', 'kratka', 'kockaste', '80.99', '11121314156', 1),
+				('kratka', 'dugačka', 'okrugle', '105.50', '12131415167', 2),
+				('kratka', 'kratka_lepršava', 'pravokutne', '99.99', '13141516178', 3);
+
+		select * from BRAT;
+
+		insert into BRAT (Suknja,Ogrlica,Asocijalno,Neprijatelj)
+	values
+			('Škotski_kilt','6','0',1),
+			('Bijelo_crveniHR kilt', '2', '0', 2),
+			('Kratka traper suknja', '3', '0', 3);
+
+			select * from SVEKAR
+
+		insert into SVEKAR (Stil_frizura, Ogrlica, asocijalno)
+			values
+				('kratka kosa', '1', '0'),
+				('duga kosa sa repom', '2', 0);
+
+
+	update PRIJATELJ set Treći_puta='2020-04-30';
+	delete from BRAT where Ogrlica<>'14';
+	select SUKNJA from CURA where Drugi_puta is null;
+
+
+select a. novčića, f.neprijatelj, e.haljina
+from ZARUČNICA a 
+inner join DEČKO_ZARUČNICA b	on b.zaručnica=a.Šifra
+inner join DEČKO c				on c.Šifra=b.Dečko
+inner join CURA d				on d.dečko=c.šifra
+inner join NEPRIJATELJ e		on e.cura=d.šifra
+inner join BRAT f			    on f.Neprijatelj=e.Šifra
+where d.Drugi_puta is not null and c.Vesta like '%ba%'
+order by e.Haljina desc;
+
+
+select distinct dečko.vesta, dečko.Asocijalno from DEČKO where Šifra not in (select šifra from DEČKO_ZARUČNICA);
