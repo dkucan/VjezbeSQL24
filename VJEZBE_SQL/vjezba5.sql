fa4b111f2926d64e111faf742b0ec8e0ab9a61af
Darko Kučan <131719007+dkucan@users.noreply.github.com>
2023-06-24 11:38:54 +0200

    vježbanje SQL-a
    vježbanje SQL-a

--- VJEZBE_SQL/vjezba5.sql
+++ VJEZBE_SQL/vjezba5.sql
@@ -1,82 +1,151 @@
-use master;
-drop database if exists VJE�BE_sql_5;
+﻿use master;
+drop database if exists VJEŽBE_sql_5;
 go
-create database VJE�BE_sql_5 collate Croatian_CI_AS;
+create database VJEŽBE_sql_5 collate Croatian_CI_AS;
 go
-use VJE�BE_sql_5;
+use VJEŽBE_sql_5;
 
-create table MLADI? (
-	�ifra inT primary key identity (1,1) not null,
+create table MLADIĆ (
+	Šifra inT primary key identity (1,1) not null,
 	Kratka_majica varchar(48) not null,
 	Haljina varchar(30) not null,
 	Asocijalno bit,
-	Zaru?nik int
+	Zaručnik int
 	);
 
-create table ZARU?NIK (
-	�ifra int primary key identity (1,1) not null,
+create table ZARUČNIK (
+	Šifra int primary key identity (1,1) not null,
 	JMBAG char(11),
 	Lipa decimal (12,8),
 	Indiferentno bit not null
 	);
 
 create table PUNAC (
-	�ifra int primary key identity (1,1) not null,
+	Šifra int primary key identity (1,1) not null,
 	Dukserica varchar(33),
-	Prviputa datetime not null,
+	Prvi_puta datetime not null,
 	Majica	varchar(36),
 	Svekar int not null
 	);
 
 create table SVEKAR (
-	�ifra int primary key identity (1,1) not null,
+	Šifra int primary key identity (1,1) not null,
 	Boja_kose varchar(33),
 	Majica varchar(31),
-	?arape varchar(31) not null,
+	Čarape varchar(31) not null,
 	Haljina varchar(43),
 	Narukvica int,
 	Eura decimal(14,5) not null
 	);
 
 create table SVEKAR_CURA (
-	�ifra int primary key identity not null,
+	Šifra int primary key identity not null,
 	Svekar int not null,
 	Cura int not null
 	);
 
 create table CURA (
-	�ifra int primary key identity not null,
-	?arape varchar(41) not null,
+	Šifra int primary key identity not null,
+	Čarape varchar(41) not null,
 	Maraka decimal(17,10) not null,
 	Asocijalno bit,
 	Vesta varchar(47) not null
 	);
 
 create table PUNICA (
-	�ifra int primary key identity not null,
-	Hla?e varchar(43) not null,
-	Nau�nica int not null,
+	Šifra int primary key identity not null,
+	Hlače varchar(43) not null,
+	Naušnica int not null,
 	Ogrlica int,
 	Vesta varchar(49) not null, 
-	Model_nao?ala varchar(31) not null,
-	Tre?i_puta datetime not null,
+	Model_naočala varchar(31) not null,
+	Treći_puta datetime not null,
 	Punac int not null
 	);
 
 create table OSTAVLJENA (
-	�ifra int primary key identity not null,
+	Šifra int primary key identity not null,
 	Majica varchar(33),
 	Ogrlica int not null,
-	?arape varchar(44),
+	Čarape varchar(44),
 	Stil_frizura varchar(42),
 	Punica int
 	);
 
-	alter table MLADI? add foreign key (ZARU?NIK) references zaru?nik (�ifra);
-	alter table PUNAC add foreign key (SVEKAR) references svekar (�ifra);
-	alter table SVEKAR_CURA add foreign key (SVEKAR) references svekar (�ifra);
-	alter table SVEKAR_CURA add foreign key (CURA) references cura (�ifra);
-	alter table PUNICA add foreign key (PUNAC) references punac (�ifra);
-	alter table OSTAVLJENA add foreign key (PUNICA) references punica (�ifra);
+	alter table MLADIĆ add foreign key (ZARUČNIK) references zaručnik (šifra);
+	alter table PUNAC add foreign key (SVEKAR) references svekar (šifra);
+	alter table SVEKAR_CURA add foreign key (SVEKAR) references svekar (šifra);
+	alter table SVEKAR_CURA add foreign key (CURA) references cura (šifra);
+	alter table PUNICA add foreign key (PUNAC) references punac (šifra);
+	alter table OSTAVLJENA add foreign key (PUNICA) references punica (šifra);
+
+	select * from SVEKAR;
+
+insert into SVEKAR (Čarape,eura)
+	values  
+	
+	('Žute',25.09),
+	('Crvene',10.10),	
+	('Smeđe',15.09);
+
+select * from PUNAC;
+
+insert into PUNAC (prvi_puta,svekar)
+ values 
+
+	('2023-06-24',3),
+	('2023-06-13',1),
+	('2023-04-24',2);
+             
+select * from PUNICA;
+
+insert into PUNICA(hlače,Naušnica,vesta,model_naočala,treći_puta,punac)
+values  
+
+	('Bijele',2,'Bijela','Okrugle_dioptrijske','2023-06-24',3),
+	('Traperice',4,'Smeđa','Kockaste_dioptrijske','2023-06-13',1),
+	('Smeđe',4,'Crna','Sunčane','2023-04-24',2);
+ 
+select * from CURA;
+
+insert into CURA(Čarape,maraka,vesta)
+	values  
+
+	('Bijele',10.00,'Bijela'),
+	('Smeđe',10.99,'Smeđa'),
+	('Crne',6.99,'Bijela');
+              
+select * from SVEKAR_CURA;
+
+insert into SVEKAR_CURA(svekar,cura)
+	values 
+
+	(3,1),
+	(1,3),
+	(2,2);
+
+select * from MLADIĆ;
+
+insert into MLADIĆ(Kratka_majica,haljina)
+	values
+
+	('Crvena','Bijela'),
+	('Bijela','Plava'),
+	('Plava','Crvena');
+
+update MLADIĆ set haljina='OSIJEK';
 
 
+select * from OSTAVLJENA;
+
+insert into OSTAVLJENA(ogrlica,punica)
+values  
+
+(21,3),
+(17,1),
+(14,2);
+        
+delete from OSTAVLJENA where Ogrlica =17; 
+
+
+select majica from punac where prvi_puta is null;
--- VJEZBE_SQL/vjezba6.sql
+++ VJEZBE_SQL/vjezba6.sql
@@ -80,4 +80,76 @@ create table BRAT (
 	alter table SVEKRVA add foreign key (PUNAC) references punac (šifra);
 	alter table BRAT add foreign key (PRIJATELJ) references prijatelj (šifra);
 
+
+ select * from PRIJATELJ;
+
+insert into PRIJATELJ(prstena,Stil_frizura)
+  values
+  
+	(2,'Dugačka'),
+	(4,'Ćelava'),
+	(6,'Kratka');
+              
+select * from BRAT;
+
+insert into BRAT(Naušnica,Treći_puta,narukvica)
+values
+
+		(8,'2023-04-09',1),
+        (5,'2023-04-24',5),
+        (8,'2023-06-24',9);      
+       
+select * from ŽENA;
+
+insert into ŽENA(novčića,narukvica,dukserica,brat)
+   values  
+
+	(20.75,2,'Plava',3),
+	(30.88,1,'Zelena',1),
+	(45.44,3,'Crna',2);        
+       
+select * from OSTAVLJENA;
+
+insert into OSTAVLJENA (prvi_puta,kratka_majica)
+values  
+
+	('2020-12-03','Plava kratka'),
+	('2019-04-23','Zelena duga'),
+	('2018-11-13','Crna kratka');
+               
+select * from PRIJATELJ;
+
+insert into PRIJATELJ_OSTAVLJENA (prijatelj,ostavljena)
+	values  
+	(2,2),
+	(1,3),
+	(3,1);
+
+
+select * from PUNAC;
+
+insert into PUNAC(ekstrovertno,suknja,majica,Prvi_puta)
+	values
+	
+	(0,'Crvena kratka','kratkih rukava','2023-06-13'),
+	(1,'Žuta kratka','bez rukava','2023-06-24'),
+	(1,'bijela kratka','kratkih','2023-05-27');
+        
+select * from SVEKRVA;
+
+insert into SVEKRVA (hlače,suknja,ogrlica,treći_puta,dukserica,narukvica,punac)
+values  
+
+	('Crvene','Bijela',1,'2023-04-24','Bijela',2,1),
+	('Bijele','Plava',2,'2023-06-23','Crvena',1,1),
+	('Plave','Crvena',3,'2023-05-27','Plava',3,1);
+        
+update svekrva set suknja='Osijek';
+
+
+delete from DEČKO where Model_naočala<'AB';
+
+select Narukvica from BRAT where Treći_puta is null;
+
+
 	
